d7a5a2f09d168339307ce68afed1fe4f1012f4b3
King-Jide <jide.remote@gmail.com>
2024-07-15 00:35:55 +0000

    addeired helmet module and declared PORT

--- myApp.js
+++ myApp.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet')
+
+const PORT = process.env.PORT || 3030;
 
 
 
@@ -57,5 +60,5 @@ app.get("/", function (request, response) {
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`🥦Useful Programmer Info Security started on Port: ${port}`);
 });
